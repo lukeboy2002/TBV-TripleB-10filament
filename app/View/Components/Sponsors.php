3061a53eaa7e9cff76b4fd82270dc0313589edfb
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2023-02-18 21:05:37 +0100

    Started Footer. Created Sponsors

---
+++ app/View/Components/Sponsors.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\Sponsor;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Sponsors extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        $sponsors = Sponsor::where('status', 1)->limit(5)->get();
+
+        return view('components.sponsors', [
+            'sponsors' => $sponsors,
+        ]);
+
+        return view('components.sponsors');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(RolesAndPermissionsSeeder::class);
+        $this->call(AdminSeeder::class);
+        $this->call(MemberSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(SponsorSeeder::class);
     }
 }
